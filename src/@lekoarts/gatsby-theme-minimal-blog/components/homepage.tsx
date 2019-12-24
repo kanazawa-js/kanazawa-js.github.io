6ba942eb684227435c58fde7af2a10f349853483
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-12-24 23:53:50 +0900

    Add files for shadowing gatsby-theme-minimal-blog

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -0,0 +1,36 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Link } from "gatsby"
+import Layout from "./layout"
+import Hero from "../texts/hero"
+import Title from "./title"
+import Listing from "./listing"
+import List from "./list"
+import useSiteMetadata from "../hooks/use-site-metadata"
+import replaceSlashes from "../utils/replaceSlashes"
+
+type PostsProps = {
+  posts: {
+    slug: string
+    title: string
+    date: string
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }[]
+}
+
+const Homepage = ({ posts }: PostsProps) => {
+  const { basePath, blogPath } = useSiteMetadata()
+
+  return (
+    <Layout>
+      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+        <Hero />
+      </section>
+    </Layout>
+  )
+}
+
+export default Homepage
