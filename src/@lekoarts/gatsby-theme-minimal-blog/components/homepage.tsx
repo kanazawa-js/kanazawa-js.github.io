d8385ab9306e9ec828f1f6a3e7b65d920ecf6b84
Kentaro Matsushita <kentaro.m811@gmail.com>
2020-02-22 19:56:28 +0900

    Remove files which used to fetch event info

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -2,25 +2,9 @@
 import { jsx } from "theme-ui"
 import Layout from "./layout"
 import Hero from "../../../components/hero"
-import Events from "../../../components/events"
 import Title from "./title"
-import useConnpassEvents from "../hooks/use-connpass-events"
-import { MDXRenderer } from "gatsby-plugin-mdx"
 
-type PostsProps = {
-  posts: {
-    slug: string
-    title: string
-    date: string
-    tags?: {
-      name: string
-      slug: string
-    }[]
-  }[]
-}
-
-const Homepage = ({ posts }: PostsProps) => {
-  const events = useConnpassEvents()
+const Homepage = () => {
   return (
     <Layout>
       <section sx={{ p: { fontSize: [1, 2, 3], mt: 2 } }}>
