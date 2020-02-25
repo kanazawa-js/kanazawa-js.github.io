3b307e4c36ff60a3da180af3b95be43aa7651eba
Kentaro Matsushita <kentaro.m811@gmail.com>
2020-02-25 13:04:30 +0900

    直近のイベント情報の表示方法を変更 (#28)
    * Update event info
    
    * Remove files which used to fetch event info

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -2,24 +2,9 @@
 import { jsx } from "theme-ui"
 import Layout from "./layout"
 import Hero from "../../../components/hero"
-import Events from "../../../components/events"
 import Title from "./title"
-import useConnpassEvents from "../hooks/use-connpass-events"
 
-type PostsProps = {
-  posts: {
-    slug: string
-    title: string
-    date: string
-    tags?: {
-      name: string
-      slug: string
-    }[]
-  }[]
-}
-
-const Homepage = ({ posts }: PostsProps) => {
-  const events = useConnpassEvents()
+const Homepage = () => {
   return (
     <Layout>
       <section sx={{ p: { fontSize: [1, 2, 3], mt: 2 } }}>
@@ -27,7 +12,7 @@ const Homepage = ({ posts }: PostsProps) => {
       </section>
       <section sx={{ p: { fontSize: [1, 2, 3], mt: 2 } }}>
         <Title text='イベント情報'/>
-        <Events events={events}/>
+        直近の開催イベントはありません。
       </section>
     </Layout>
   )
